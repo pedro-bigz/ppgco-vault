d5146dd61db376d2d21d8208587d95663eb8ab10
Pedro Big Z <pedropaulo.spaiva.a@gmail.com>
2024-04-03 06:27:01 -0300

    install.sh  correcao

--- install.sh
+++ install.sh
@@ -3,9 +3,7 @@ sudo chmod -R 755 /var/www/ &&
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash &&
 
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-
-&&
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh";
 
 nvm install 20.10.0 &&
 nvm  use 20.10.0 &&
