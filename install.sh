6534d511c668fba210b3f448bf48b521e45a1eef
Pedro Big Z <pedropaulo.spaiva.a@gmail.com>
2024-04-03 06:25:30 -0300

    install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+sudo chmod -R 755 /var/www/ &&
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash &&
+
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
+&&
+
+nvm install 20.10.0 &&
+nvm  use 20.10.0 &&
+
+npm install -g pnpm &&
+npm install -g pm2 &&
