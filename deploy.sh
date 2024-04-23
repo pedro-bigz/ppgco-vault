6d885bad94bf67eca1b01ffdb921c4661aeff9b6
Pedro Big Z <pedropaulo.spaiva.a@gmail.com>
2024-04-23 18:43:07 -0300

    teste chmod

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
+sudo chmod -R 777 /var/www &&
 source ~/.nvm/nvm.sh &&
 nvm use 20.10.0 &&
 cd /var/www/ppgco-vault &&
 git reset --hard origin/main &&
 git checkout main &&
 git pull origin main &&
-/bin/bash /var/www/ppgco-vault/$1-ppgco-ufu/prod/deploy.sh
+/bin/bash /var/www/ppgco-vault/$1-ppgco-ufu/prod/deploy.sh &&
+sudo chmod -R 755 /var/www
