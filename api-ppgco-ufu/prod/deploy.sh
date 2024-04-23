e2d1989ca70219251f070e0250c411d8caa4fbc5
Pedro Big Z <pedropaulo.spaiva.a@gmail.com>
2024-04-23 16:51:34 -0300

    updating env

--- api-ppgco-ufu/prod/deploy.sh
+++ api-ppgco-ufu/prod/deploy.sh
@@ -13,11 +13,11 @@ sudo cp /var/www/ppgco-vault/api-ppgco-ufu/prod/.env.prod /var/www/prod/api-ppgc
 pnpm build &&
 
 if [ "$(pm2 id api-ppgco-ufu) "= "[]" ]; then
-  pm2 start dist/src/main.js --name api-ppgco-ufu
+  pm2 start dist/main.js --name api-ppgco-ufu
   pm2 save
 else
   pm2 stop api-ppgco-ufu
   pm2 delete api-ppgco-ufu
-  pm2 start dist/src/main.js --name api-ppgco-ufu
+  pm2 start dist/main.js --name api-ppgco-ufu
   pm2 save
 fi
