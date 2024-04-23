e3a865a1e0be91a4108c7e6eb803f1ae5ae27805
Pedro Big Z <pedropaulo.spaiva.a@gmail.com>
2024-04-23 18:41:06 -0300

    forcing pull

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 source ~/.nvm/nvm.sh &&
 nvm use 20.10.0 &&
 cd /var/www/ppgco-vault &&
+git reset --hard origin/main &&
 git checkout main &&
 git pull origin main &&
 /bin/bash /var/www/ppgco-vault/$1-ppgco-ufu/prod/deploy.sh
